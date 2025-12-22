4494674099e37c267c4e7c627a152abf1015ade9
OmRakhade <omanilrakhade07@gmail.com>
2025-12-22 15:52:53 +0530

    solve rec problem, get the count of subsequences of array whoes sum equals to given value.

--- day7.java
+++ day7.java
@@ -5,8 +5,11 @@ public class day7 {
     public static void main(String[] args) {
         recursionProblems r = new recursionProblems();
         int[] arr = {1,2,1};
+        int sum = 2;
         // r.getAllSubsequences(0,arr,new ArrayList<>());
-        r.getSubsequnces(0, arr, 2, new ArrayList<>(),0);
+        // r.getSubsequnces(0, arr, sum, new ArrayList<>(),0);
+        int count = r.getCoutnOfSubsequences(0, arr, sum, 0, 0);
+        System.out.println("COUNT = "+count);
     }
 }
 
@@ -55,5 +58,24 @@ class recursionProblems{
         //  move forward without element
         getSubsequnces(ind+1, arr, sum, current, csum);
 
+    }
+
+    // problem :- count of subsequences having sum = k.
+    // ex:- arr = {1,2,1}, sum = 2 =>total count = 2
+
+    int getCoutnOfSubsequences(int ind,int[] arr, int sum, int cSum, int count){
+
+        if(ind>=arr.length){
+            if(cSum == sum){
+                return 1;
+            }
+            return 0;
+        }
+        
+        int left =  getCoutnOfSubsequences(ind+1, arr, sum, cSum+arr[ind], count);
+
+        int right = getCoutnOfSubsequences(ind+1, arr, sum, cSum, count);
+
+        return left+right;
     }
 }
